9b48c396a020697882e880bb42d215a00123d9cf
Ario <ario.hitmeup@gmail.com>
2025-10-16 21:00:05 +0700

    Updated feature tests

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -27,6 +27,7 @@ class TaskTest extends TestCase
         $user = User::factory()->create();
         $task = Task::factory()->create([
             'assigned_user_id' => $user->id,
+            'created_by_user_id' => $user->id,
         ]);
         $response = $this->actingAs($user)->get('api/tasks/' . $task->id);
 
@@ -40,6 +41,7 @@ class TaskTest extends TestCase
             'title' => 'Test Task',
             'description' => 'Test Task Description',
             'assigned_user_id' => $user->id,
+            'created_by_user_id' => $user->id,
             'status' => 'pending',
             'priority' => 'high',
             'due_date' => now()->addDays(7),
@@ -53,11 +55,13 @@ class TaskTest extends TestCase
         $user = User::factory()->create();
         $task = Task::factory()->create([
             'assigned_user_id' => $user->id,
+            'created_by_user_id' => $user->id,
         ]);
         $response = $this->actingAs($user)->put('api/tasks/' . $task->id, [
             'title' => 'Test Task',
             'description' => 'Test Task Description',
             'assigned_user_id' => $user->id,
+            'created_by_user_id' => $user->id,
             'status' => 'pending',
             'priority' => 'high',
             'due_date' => now()->addDays(7),
@@ -71,6 +75,7 @@ class TaskTest extends TestCase
         $user = User::factory()->create();
         $task = Task::factory()->create([
             'assigned_user_id' => $user->id,
+            'created_by_user_id' => $user->id,
         ]);
         $response = $this->actingAs($user)->delete('api/tasks/' . $task->id);
 
