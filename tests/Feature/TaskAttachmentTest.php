f841ddfb74d3d9d9904c5ea1230fc6249f62c0f7
Ario <ario.hitmeup@gmail.com>
2025-10-16 21:26:59 +0700

    Feat: added tests for unauthenticated users on tasks

--- tests/Feature/TaskAttachmentTest.php
+++ tests/Feature/TaskAttachmentTest.php
@@ -5,16 +5,30 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Task;
+use Illuminate\Support\Facades\Storage;
 
 class TaskAttachmentTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test_the_application_can_attach_a_file_to_a_task(): void
     {
-        $response = $this->get('/');
+        $user = User::factory()->create();
+        $task = Task::factory()->create([
+            'assigned_user_id' => $user->id,
+            'created_by_user_id' => $user->id,
+        ]);
+
+        Storage::fake('public');
+        $response = $this->actingAs($user)->post('/api/tasks/' . $task->id . '/attachments', [
+            'file' => 'test.jpg',
+        ]);
 
         $response->assertStatus(200);
     }
+
+
 }
--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -34,7 +34,7 @@ class TaskTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_the_application_can_store_task(): void
+    public function test_the_application_can_store_a_task(): void
     {
         $user = User::factory()->create();
         $response = $this->actingAs($user)->post('api/tasks', [
@@ -50,7 +50,7 @@ class TaskTest extends TestCase
         $response->assertStatus(201);
     }
 
-    public function test_the_application_can_update_task(): void
+    public function test_the_application_can_update_a_task(): void
     {
         $user = User::factory()->create();
         $task = Task::factory()->create([
@@ -70,7 +70,7 @@ class TaskTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_the_application_can_delete_task(): void
+    public function test_the_application_can_delete_a_task(): void
     {
         $user = User::factory()->create();
         $task = Task::factory()->create([
@@ -80,5 +80,61 @@ class TaskTest extends TestCase
         $response = $this->actingAs($user)->delete('api/tasks/' . $task->id);
 
         $response->assertStatus(204);
+    }
+
+    public function test_the_application_doesnt_allow_unauthenticated_users_to_access_tasks(): void
+    {
+        $response = $this->get('api/tasks');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_the_application_doesnt_allow_unauthenticated_users_to_store_a_task(): void
+    {
+        $user = User::factory()->create();
+        $response = $this->post('api/tasks', [
+            'title' => 'Test Task',
+            'description' => 'Test Task Description',
+            'assigned_user_id' => $user->id,
+            'created_by_user_id' => $user->id,
+            'status' => 'pending',
+            'priority' => 'high',
+            'due_date' => now()->addDays(7),
+        ]);
+
+        $response->assertStatus(401);
+    }
+
+    public function test_the_application_doesnt_allow_unauthenticated_users_to_update_a_task(): void
+    {
+        $user = User::factory()->create();
+        $task = Task::factory()->create([
+            'assigned_user_id' => $user->id,
+            'created_by_user_id' => $user->id,
+        ]);
+        
+        $response = $this->put('api/tasks/' . $task->id, [
+            'title' => 'Test Task',
+            'description' => 'Test Task Description',
+            'assigned_user_id' => $user->id,
+            'created_by_user_id' => $user->id,
+            'status' => 'pending',
+            'priority' => 'high',
+            'due_date' => now()->addDays(7),
+        ]);
+
+        $response->assertStatus(401);
+    }
+
+    public function test_the_application_doesnt_allow_unauthenticated_users_to_delete_a_task(): void
+    {
+        $user = User::factory()->create();
+        $task = Task::factory()->create([
+            'assigned_user_id' => $user->id,
+            'created_by_user_id' => $user->id,
+        ]);
+        $response = $this->delete('api/tasks/' . $task->id);
+
+        $response->assertStatus(401);
     }
 }
