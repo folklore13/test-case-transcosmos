2e2f3c59415a603fcc87f0f149eff83ee3578113
Ario <ario.hitmeup@gmail.com>
2025-10-16 18:48:56 +0700

    Feat: defined schemas for tasks, task attachments, and task comments

---
+++ database/migrations/2025_10_16_113954_create_task_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('task_comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignUuid('task_id')
+                ->constrained('tasks')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreignUuid('user_id')
+                ->constrained('users')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->string('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task_comments');
+    }
+};
